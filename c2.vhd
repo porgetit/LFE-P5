fdcfb082b7c7f0abce2a8156c1bcd40b261508e6
Kevin Esguerra Cardona <kevin.esguerra@utp.edu.co>
2025-04-21 00:33:17 -0500

    Create c2.vhd

---
+++ c2.vhd
@@ -0,0 +1,17 @@
+-- c2.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity C2 is
+    port (
+        S1 : in  std_logic;
+        S2 : in  std_logic;
+        S3 : in  std_logic;
+        S4 : in  std_logic;
+        S5 : in  std_logic;
+        D0 : out std_logic;
+        D1 : out std_logic;
+        D2 : out std_logic;
+        D3 : out std_logic
+    );
+end entity C2;
