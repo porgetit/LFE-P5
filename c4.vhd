72456b4a22334b938df2e6a646c9f2a8e2b420af
Kevin Esguerra Cardona <kevin.esguerra@utp.edu.co>
2025-04-21 19:30:11 -0500

    Update c4.vhd

--- c4.vhd
+++ c4.vhd
@@ -1,4 +1,4 @@
--- c4.vhd
+-- c4_sop.vhd
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -9,9 +9,22 @@ entity C4 is
         S3 : in  std_logic;
         S4 : in  std_logic;
         S5 : in  std_logic;
-        D0 : out std_logic;
-        D1 : out std_logic;
-        D2 : out std_logic;
         D3 : out std_logic
     );
 end entity C4;
+
+architecture behavior of C4 is
+    signal D31, D32, D33, D34, D35, D36, D37, D38, D39, D310 : std_logic;
+begin
+    D31 <= (S1 or S2 or (not S3) or S4 or (not S5));
+    D32 <= (S1 or S2 or (not S3) or (not S4) or (not S5);
+    D33 <= (S1 or (not S2) or (not S3) or S4 or (not S5));
+    D34 <= (S1 or (not S2) or (not S3) or (not S4) or (not S5));
+    D35 <= ((not S1) or (not S2) or S3 or S4 or (not S5));
+    D36 <= ((not S1) or (not S2) or S3 or (not S4) or (not S5));
+    D37 <= ((not S1) or (not S2) or (not S3) or S4 or S5);
+    D38 <= ((not S1) or (not S2) or (not S3) or S4 or (not S5));
+    D39 <= ((not S1) or (not S2) or (not S3) or (not S4) or S5);
+    D310 <= ((not S1) or (not S2) or (not S3) or (not S4) or (not S5));
+    D3 <= D31 and D32 and D33 and D34 and D35 and D36 and D37 and D38 and D39 and D310;
+end behavior;
