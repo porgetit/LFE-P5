e6d427a4740532f0b280fc6ea010b79e56a872aa
Kevin Esguerra Cardona <kevin.esguerra@utp.edu.co>
2025-04-21 19:49:17 -0500

    Update main.vhd

--- main.vhd
+++ main.vhd
@@ -1,43 +1,43 @@
--- circuito_principal.vhd
+-- main.vhd
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity Circuito_Principal is
+entity Main is
     port (
-        -- Entradas comunes
+        -- Entradas 
         S1 : in  std_logic;
         S2 : in  std_logic;
         S3 : in  std_logic;
         S4 : in  std_logic;
         S5 : in  std_logic;
-        -- Salidas comunes
+        -- Salidas 
         D0 : out std_logic;
         D1 : out std_logic;
         D2 : out std_logic;
         D3 : out std_logic
     );
-end entity Circuito_Principal;
+end entity Main;
 
-architecture Structural of Circuito_Principal is
+architecture Structural of Main is
 
     ------------------------------------------------------------------
     --  Declaración de componentes (se enlazan con las entidades C1‑C4)
     ------------------------------------------------------------------
     component C1
         port ( S1,S2,S3,S4,S5 : in  std_logic;
-               D0,D1,D2,D3    : out std_logic );
+               D0             : out std_logic );
     end component;
     component C2
         port ( S1,S2,S3,S4,S5 : in  std_logic;
-               D0,D1,D2,D3    : out std_logic );
+              D1              : out std_logic );
     end component;
     component C3
         port ( S1,S2,S3,S4,S5 : in  std_logic;
-               D0,D1,D2,D3    : out std_logic );
+               D2             : out std_logic );
     end component;
     component C4
         port ( S1,S2,S3,S4,S5 : in  std_logic;
-               D0,D1,D2,D3    : out std_logic );
+               D3             : out std_logic );
     end component;
 
     ------------------------------------------------------------------
